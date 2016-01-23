e82fd5c7d5a514c28e24faed9ddd2941327cdd00
Daniel Woznicki <tokay89@sbcglobal.net>
2016-01-23 14:01:39 -0800

    Connect tags to messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,18 +9,21 @@ end
 # Seed meetup data
 meetup_api = MeetupApi.new
 
-art_params = {
-	category: '1',
-	country: 'us',
-	state: 'CA',
-	city: 'San Francisco',
-	format: 'json',
-	page: '15'
-}
-art_events = meetup_api.open_events(art_params)
-
-art_events['results'].each do |event|
-	organization = Organization.find_or_create_by(name: event['group']['name'], url: "www.meetup.com/#{event['group']['urlname']}")
-	Message.create(body: event['name'], start_date: Time.at(event['time']), organization_id: organization.id)
+category_ids = ["1", "4", "6", "13", "12", "10"]
+category_ids.each do |id|
+	params = {
+		category: id,
+		country: 'us',
+		state: 'CA',
+		city: 'San Francisco',
+		format: 'json',
+		page: '15'
+	}
+	events = meetup_api.open_events(params)
+	events['results'].each do |event|
+		organization = Organization.find_or_create_by(name: event['group']['name'], url: "www.meetup.com/#{event['group']['urlname']}")
+		message = Message.create(body: event['name'], start_date: Time.at(event['time']), organization_id: organization.id)
+	end
 end
+
 
