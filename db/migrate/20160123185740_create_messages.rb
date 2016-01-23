3516713971d6875552ebbcbb702291630e080b89
Daniel Woznicki <tokay89@sbcglobal.net>
2016-01-23 11:16:49 -0800

    Create models and tables

---
+++ db/migrate/20160123185740_create_messages.rb
@@ -0,0 +1,12 @@
+class CreateMessages < ActiveRecord::Migration
+  def change
+    create_table :messages do |t|
+    	t.string :body
+    	t.date :start_date
+    	t.date :end_date
+    	t.references :organization
+
+      t.timestamps null: false
+    end
+  end
+end
