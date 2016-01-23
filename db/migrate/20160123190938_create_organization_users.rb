f41c97f966189284f1d61f14e8bb3a4bca14a820
Daniel Woznicki <tokay89@sbcglobal.net>
2016-01-23 11:18:50 -0800

    Set up join tables

--- db/migrate/20160123190938_create_organization_users.rb
+++ db/migrate/20160123190938_create_organization_users.rb
@@ -1,6 +1,8 @@
 class CreateOrganizationUsers < ActiveRecord::Migration
   def change
     create_table :organization_users do |t|
+    	t.references :organization
+    	t.references :user
 
       t.timestamps null: false
     end
--- db/migrate/20160123191005_create_tag_users.rb
+++ db/migrate/20160123191005_create_tag_users.rb
@@ -1,6 +1,8 @@
 class CreateTagUsers < ActiveRecord::Migration
   def change
     create_table :tag_users do |t|
+    	t.references :tag
+    	t.references :user
 
       t.timestamps null: false
     end
