3d14d9840fd049a27ab15484a1842ba8ee04f8ff
paulozag <ozag@comcast.net>
2016-01-23 13:14:50 -0800

    database relations seem to hold, seeded some dummy info to manipulate

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
 
   has_many :tag_users
   has_many :tags, through: :tag_users
+  has_many :messages, through: :tags
 
   has_many :organization_users
   has_many :organizations, through: :organization_users
